99d81516c097606f220bf06101e0afffa65b8125
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-01-21 13:02:37 +0900

    feat: 대문자 알파벳을 배열에 넣고 출력하는 기능 구현

--- Alphabet.java
+++ Alphabet.java
@@ -1,2 +1,17 @@
 public class Alphabet {
+    public static void main(String[] args) {
+        char[] words = new char[26];
+
+        for (int i = 0; i < 26; i++) {
+            words[i] = (char) ('A' + i);
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            if (i == words.length - 1) {
+                System.out.print(words[i]);
+                continue;
+            }
+            System.out.print(words[i] + ", ");
+        }
+    }
 }
